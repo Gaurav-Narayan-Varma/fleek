67669e17fd25ed311731d514554fb34a021d6d2f
Gaurav Varma <gaurav.narayan.varma@gmail.com>
2023-03-16 19:21:45 -0700

    extracted navbar into separate function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,17 @@
-import { Routes, Route, Link } from "react-router-dom";
-import fleekLogo from "./assets/black_logo_transparent_background.avif";
+import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Charts from "./pages/Charts";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const location = useLocation();
-  const dummy = true;
-
   return (
-    <div className="App">
-      <div className="flex">
-        <section
-          id="nav-bar"
-          className="px-5 w-56 shrink-0 h-auto bg-white text-black font-sans border border-stone-200"
-        >
-          <img
-            src={fleekLogo}
-            className="h-20 w-auto"
-            alt="Fleek logo with transparent background"
-          />
-          <ul className="">
-            <li className={`${location.pathname === "/" && "font-bold"} mb-4`}>
-              <Link to="/">📦 Parcel Dashboard</Link>
-            </li>
-            <li className={`${location.pathname === "/data" && "font-bold"}`}>
-              <Link to="/data">📈 Charts & Analysis</Link>
-            </li>
-          </ul>
-        </section>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/data" element={<Charts />} />
-        </Routes>
-      </div>
+    <div className="App flex">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/data" element={<Charts />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import fleekLogo from "../assets/black_logo_transparent_background.avif";
+
+export default function NavBar() {
+  return (
+    <section
+      id="nav-bar"
+      className="px-5 w-56 shrink-0 h-auto bg-white text-black font-sans border border-stone-200"
+    >
+      <img
+        src={fleekLogo}
+        className="h-20 w-auto"
+        alt="Fleek logo with transparent background"
+      />
+      <ul className="">
+        <li className={`${location.pathname === "/" && "font-bold"} mb-4`}>
+          <Link to="/">📦 Parcel Dashboard</Link>
+        </li>
+        <li className={`${location.pathname === "/data" && "font-bold"}`}>
+          <Link to="/data">📈 Charts & Analysis</Link>
+        </li>
+      </ul>
+    </section>
+  );
+}
